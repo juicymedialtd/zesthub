f898ae88258cd2cd7ae00899d5a64085f6f8576e
Jack Andrews <japotts16@gmail.com>
2022-10-05 11:15:45 +0100

    mileage

--- pages/mileage/index.js
+++ pages/mileage/index.js
@@ -2,7 +2,7 @@ export default function Miles() {
   return (
     <div className="py-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <h1 className="text-2xl font-semibold text-gray-900">Milage Tracking</h1>
+        <h1 className="text-2xl font-semibold text-white">Milage Tracking</h1>
       </div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         <div className="py-4">
@@ -10,7 +10,7 @@ export default function Miles() {
             <a
               type="button"
               href="/mileage/new"
-              className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
+              className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-gray-900 bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
             >
               Add a new trip
             </a>
--- pages/mileage/new.js
+++ pages/mileage/new.js
@@ -1,23 +1,83 @@
 export default function NewTrip() {
-    return (
-      <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">Add a new trip</h1>
+  return (
+    <div className="py-6">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <h1 className="text-2xl font-semibold text-white">Add a new trip</h1>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="py-4">
+          <div className="float-right">
+            <a
+              type="button"
+              href="/mileage"
+              className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-gray-900 bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
+            >
+              Back
+            </a>
+          </div>
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <div className="py-4">
-            <div className="float-right">
-              <a
-                type="button"
-                href="/mileage"
-                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
-              >
-                Back
-              </a>
+
+        <div className="mt-12 ">
+          <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+            <div className="px-4 py-5 sm:p-6">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm text-gray-900 font-bold"
+                >
+                  Reason
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="block w-full border-gray-900 shadow-sm border-2 rounded-md sm:text-sm"
+                    placeholder="Type your reason here"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col mt-8 space-y-3">
+                <div className="w-1/2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-bold text-gray-900"
+                  >
+                    Start Destination
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="m50 2st"
+                    />
+                  </div>
+                </div>
+                <div className="w-1/2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    End Destination
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="you@example.com"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
