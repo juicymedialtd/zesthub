d9d4318bedaa5ed5a7629cab5028ef8e37c7bdcc
Jack Andrews <japotts16@gmail.com>
2022-10-12 15:02:36 +0100

    support teams

--- prisma/migrations/20221012140208_/migration.sql
+++ prisma/migrations/20221012140208_/migration.sql
@@ -8,6 +8,16 @@ CREATE TYPE "Status" AS ENUM ('pending', 'approved', 'denied');
 CREATE TYPE "HolidayType" AS ENUM ('annual', 'health', 'training', 'other');
 
 -- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -18,6 +28,7 @@ CREATE TABLE "User" (
     "holidaysLeft" INTEGER NOT NULL DEFAULT 21,
     "HolidayAllowance" INTEGER NOT NULL DEFAULT 21,
     "role" "Role" NOT NULL DEFAULT E'USER',
+    "teamId" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -83,7 +94,13 @@ CREATE TABLE "Wiki" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Team_name_key" ON "Team"("name");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Holiday" ADD CONSTRAINT "Holiday_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,14 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
+  const init = await prisma.team.upsert({
+    where: { name: "test" },
+    update: {},
+    create: {
+      name: 'test'
+    },
+  });
+
   const admin = await prisma.user.upsert({
     where: { email: "admin@admin.com" },
     update: {},
@@ -10,6 +18,7 @@ async function main() {
       name: "admin",
       role: "ADMIN",
       password: "$2b$10$BFmibvOW7FtY0soAAwujoO9y2tIyB7WEJ2HNq9O7zh9aeejMvRsKu",
+      teamId: init.id,
     },
   });
 
