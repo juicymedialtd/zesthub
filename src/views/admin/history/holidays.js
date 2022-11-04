26b1a02af6bfa15bf1940efa60e90cfa79c39376
Jack Andrews <japotts16@gmail.com>
2022-11-04 00:39:13 +0000

    history table sortable

--- src/views/admin/history/holidays.js
+++ src/views/admin/history/holidays.js
@@ -1,5 +1,107 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { format, parseISO } from "date-fns";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  createColumnHelper,
+} from "@tanstack/react-table";
+
+function DefaultColumnFilter({ column: { filterValue, setFilter } }) {
+  return (
+    <input
+      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+      type="text"
+      value={filterValue || ""}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+      }}
+      placeholder="Type to filter"
+    />
+  );
+}
+
+function Table({ columns, data }) {
+  const [sorting, setSorting] = useState([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+  });
+
+  return (
+    <>
+      <table className="min-w-full divide-y divide-gray-300">
+        <thead className="bg-topnav-bg">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    className="py-2 pl-4 pr-3 text-sm text-left font-semibold text-white sm:pl-6"
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? "cursor-pointer select-none"
+                            : "",
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: "",
+                          desc: "",
+                        }[header.column.getIsSorted()] ?? null}
+                      </div>
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td
+                      key={cell.id}
+                      className="px-6 py-2 text-left text-sm text-gray-500 whitespace-nowrap"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
 
 export default function AdminAllHolidays() {
   const [loading, setLoading] = useState(true);
@@ -23,109 +125,53 @@ export default function AdminAllHolidays() {
     getHolidays();
   }, []);
 
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.display({
+      id: "createdBy",
+      header: "createdBy",
+      accessorKey: "User.name",
+    }),
+    columnHelper.display({
+      id: "type",
+      header: "Reason",
+      accessorKey: "type",
+    }),
+    columnHelper.display({
+      id: "status",
+      header: "Status",
+      accessorKey: "status",
+    }),
+    columnHelper.display({
+      id: "StartDate",
+      header: "Start Date",
+      accessorFn: (row) => `${format(parseISO(row.startDate), "dd/MM/yyyy")}`,
+    }),
+    columnHelper.display({
+      id: "endDate",
+      header: "End Date",
+      accessorFn: (row) => `${format(parseISO(row.endDate), "dd/MM/yyyy")}`,
+    }),
+    columnHelper.display({
+      id: "status",
+      header: "Status",
+      accessorKey: "status",
+    }),
+    columnHelper.display({
+      id: "used",
+      header: "Days Used",
+      accessorKey: "daysUsed",
+    }),
+  ];
+
   return (
     <>
       {!loading && (
         <div>
           {holidays.length > 0 && (
             <div>
-              <div>
-                <div className="mt-8 flex flex-col">
-                  <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                      <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                        <table className="min-w-full divide-y divide-gray-300">
-                          <thead className="bg-topnav-bg">
-                            <tr>
-                              <th
-                                scope="col"
-                                className="py-2 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-6"
-                              >
-                                Created By
-                              </th>
-                              <th
-                                scope="col"
-                                className="py-2 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-6"
-                              >
-                                Reason
-                              </th>
-                              <th
-                                scope="col"
-                                className="hidden px-3 py-2 text-right text-sm font-semibold text-white lg:table-cell"
-                              >
-                                Status
-                              </th>
-                              <th
-                                scope="col"
-                                className="hidden px-3 py-2 text-right text-sm font-semibold text-white lg:table-cell"
-                              >
-                                Start Date
-                              </th>
-                              <th
-                                scope="col"
-                                className="hidden px-3 py-2 text-right text-sm font-semibold text-white sm:table-cell"
-                              >
-                                End Date
-                              </th>
-                              <th
-                                scope="col"
-                                className="px-3 py-2 text-right text-sm font-semibold text-white"
-                              >
-                                Days Used
-                              </th>
-                            </tr>
-                          </thead>
-                          <tbody className="divide-y divide-gray-200 bg-white">
-                            {holidays.map((item) => (
-                              <tr key={item.id}>
-                                <td className="px-3 text-right py-2 text-sm text-gray-500 whitespace-nowrap">
-                                  {item.User.name}
-                                </td>
-                                <td className="w-3/4 max-w-0 py-2 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6 capitalize">
-                                  {item.type}
-                                  <dl className="font-normal lg:hidden">
-                                    <dt className="mt-1">
-                                      Status - {item.status}
-                                    </dt>
-                                    <dd className="mt-1 truncate text-gray-700">
-                                      {format(
-                                        parseISO(item.startDate),
-                                        "dd/MM/yyyy"
-                                      )}
-                                    </dd>
-                                    <dt className="sr-only sm:hidden">Email</dt>
-                                    <dd className="mt-1 truncate text-gray-500 sm:hidden">
-                                      {format(
-                                        parseISO(item.endDate),
-                                        "dd/MM/yyyy"
-                                      )}
-                                    </dd>
-                                  </dl>
-                                </td>
-                                <td className="hidden text-right px-3 py-2 text-sm text-gray-500 lg:table-cell capitalize">
-                                  {item.status}
-                                </td>
-                                <td className="hidden text-right px-3 py-2 text-sm text-gray-500 lg:table-cell">
-                                  {format(
-                                    parseISO(item.startDate),
-                                    "dd/MM/yyyy"
-                                  )}
-                                </td>
-                                <td className="hidden text-right px-3 py-2 text-sm text-gray-500 sm:table-cell">
-                                  {format(parseISO(item.endDate), "dd/MM/yyyy")}
-                                </td>
-                                <td className="px-3 text-right py-2 text-sm text-gray-500 whitespace-nowrap">
-                                  {item.daysUsed}
-                                </td>
-                              </tr>
-                            ))}
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Table data={holidays} columns={columns} />
             </div>
           )}
         </div>
