2bb6e1ed9a61d120e182939d4c40412a32d3082e
Jack Andrews <japotts16@gmail.com>
2022-11-06 23:34:40 +0000

    favicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -47,14 +47,34 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <Head>
         <title>ZestHub</title>
         <meta name="description" content="Internal HR management" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <SessionProvider session={session}>
         <QueryClientProvider client={queryClient}>
           <Auth>
-              <SideLayout>
-                <Component {...pageProps} />
-              </SideLayout>
+            <SideLayout>
+              <Component {...pageProps} />
+            </SideLayout>
           </Auth>
         </QueryClientProvider>
       </SessionProvider>
